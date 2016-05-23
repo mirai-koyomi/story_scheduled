74807e85cc522073ddb2bab1595bf06a227412d0
mirai_koyomi <crossclose00@gmail.com>
2016-05-24 07:47:41 +0900

    db migrate

--- db/migrate/20160523215245_create_worlds.rb
+++ db/migrate/20160523215245_create_worlds.rb
@@ -1,6 +1,9 @@
 class CreateWorlds < ActiveRecord::Migration
   def change
     create_table :worlds do |t|
+      t.string :world_name, null: false
+      t.string :description
+      t.string :status, null: false, default: 'draft'
 
       t.timestamps null: false
     end
