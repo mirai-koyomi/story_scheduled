f570734b6f2ff2e86bbf090f35e3193925e66d5f
mirai_koyomi <crossclose00@gmail.com>
2016-05-24 08:22:14 +0900

    create test

--- test/models/world_test.rb
+++ test/models/world_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class WorldTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # statusがdraft, public, private以外の場合はエラー
+  test "status_test" do
+    thorn_chester = FactoryGirl.build(:world, status: "public")
+    risomata = FactoryGirl.build(:world, status: "ganma")
+
+    assert thorn_chester.invalid?
+    assert risomata.valid?
+
+    assert risomata.errors.include?(:status)
+  end
+
+  test "description test" do
+    risomata = FactoryGirl.build(:world, description: "A" * 501)
+
+    assert risomata.valid?
+
+    assert risomata.errors.include?(:description)
+  end
 end
